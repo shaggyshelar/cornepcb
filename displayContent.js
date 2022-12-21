450d98733aa177511ca61470b811f6a5ce8b5636
Sagar Shelar <sagarshelar@Sagars-MacBook-Pro.local>
2022-12-21 20:29:31 +0530

    Minor updates

--- displayContent.js
+++ displayContent.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
 const readline = require("readline");
+console.log(process.argv[2]);
 
 async function processLineByLine() {
-  const fileStream = fs.createReadStream(
-    "./node_modules/ergogen/src/footprints/index.js"
-  );
+  // const fileStream = fs.createReadStream(
+  //   "./node_modules/ergogen/src/footprints/index.js"
+  // );
+
+  const fileStream = fs.createReadStream(process.argv[2]);
 
   const rl = readline.createInterface({
     input: fileStream,
